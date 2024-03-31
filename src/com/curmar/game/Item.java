db3b4f88b5e4a267f394ac12cb616f81a95d8f13
AlexR <cursedmaria@mail.ru>
2024-03-31 20:29:59 +0700

    Added item logic
    name and rare and getters/setters

--- src/com/curmar/game/Item.java
+++ src/com/curmar/game/Item.java
@@ -1,7 +1,20 @@
 package com.curmar.game;
 
 
-public class Item
-{
+public class Item {
+    private final String name;
+    private final int rare;
 
+    public Item(String name, int rare) {
+        this.name = name;
+        this.rare = rare;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRare() {
+        return rare;
+    }
 }
