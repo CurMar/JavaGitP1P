0357052835b7b1b0670b46c03ed106a2d6290424
AlexR <cursedmaria@mail.ru>
2024-03-31 20:37:55 +0700

    Player items prin

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
-
-
+import com.curmar.game.*;
 
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("<-BEGIN->");
+        Player pl = new Player();
+        pl.getInventory().add(new Item("Sword", 2));
+        pl.getInventory().add(new Item("Axe", 5));
+
+        for (Item i : pl.getInventory()) {
+            System.out.println(i.getName());
+        }
     }
 }
